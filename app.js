3532c3e5e494ed35f86f1f429018213074c3e1d1
Stephane Dorotich <stephanedorotich@gmail.com>
2021-05-28 17:29:28 -0600

    v2.2 Updated nav bar for registration page. New jitter fix for bg image

--- app.js
+++ app.js
@@ -3,8 +3,23 @@ const parallax_height = ($(window).height())+"px";
 var parallax_images = document.getElementsByClassName('parallax');
 
 for(i = 0; i<parallax_images.length; i++) {
+    var w = parallax_images[i].offsetWidth;
     var h = parallax_images[i].offsetHeight;
-    if (h == 0 || h < parallax_height) {
-        parallax_images[i].style.height = parallax_height;
+
+    var image = new Image();
+    image.src = '/images/bowhut_2019.jpg';
+
+    var img_aspct = image.width/ image.height;
+    var act_aspct = w / h;
+
+    if (act_aspct >= img_aspct) {
+        parallax_images[i].style.backgroundSize = "100% auto";
+    } else {
+        if(w * img_aspct > h) {
+            parallax_images[i].style.backgroundSize = "100% auto";
+        } else {
+            parallax_images[i].style.backgroundSize = "auto 100vh";
+        }
     }
+    
 }
