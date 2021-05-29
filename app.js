328cccc9205967664b5e6dea6612e2f7ea10ef2b
Stephane Dorotich <stephanedorotich@gmail.com>
2021-05-28 19:34:21 -0600

    v2.2 Corrected Parallax image sizing. Attempt position: center top.

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const parallax_height = ($(window).height())+"px";
 var parallax_images = document.getElementsByClassName('parallax');
 
 for(i = 0; i<parallax_images.length; i++) {
-    var w = parallax_images[i].offsetWidth;
-    var h = parallax_images[i].offsetHeight;
+    const w = $(window).width();
+    const h = $(window).height();
 
     var image = new Image();
     image.src = '/images/bowhut_2019.jpg';
@@ -13,13 +13,9 @@ for(i = 0; i<parallax_images.length; i++) {
     var act_aspct = w / h;
 
     if (act_aspct >= img_aspct) {
-        parallax_images[i].style.backgroundSize = "100% auto";
+        parallax_images[i].style.backgroundSize = `${w+"px"} auto`;
     } else {
-        if(w * img_aspct > h) {
-            parallax_images[i].style.backgroundSize = "100% auto";
-        } else {
-            parallax_images[i].style.backgroundSize = "auto 100vh";
-        }
+        parallax_images[i].style.backgroundSize = `auto ${h+"px"}`;
     }
     
 }
