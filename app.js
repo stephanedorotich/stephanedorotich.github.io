421d82dbebb209e5922e749fc9a8715d2f250463
Stephane Dorotich <stephanedorotich@gmail.com>
2021-08-17 20:43:14 -0600

    Content update. Menu hide after clicking link.

--- app.js
+++ app.js
@@ -10,3 +10,32 @@ $( window ).scroll(function() {
         $("#bg2" ).css( "z-index", -1);
     }
   });
+
+// Anchor padding
+//  Adjusts padding/margins on anchors to accommodate navbar and navbar toggle
+//  Ensures that links scroll to top of anchored sections
+function anchorPadding() {
+    var anchors = document.getElementsByClassName("anchor");
+    if (window.innerWidth < 768) {
+        for (var i = 0; i < anchors.length; i++) {
+            anchors[i].style.paddingTop = "215px";
+            anchors[i].style.marginTop = "-215px";
+        }
+    } else {
+        for (var i = 0; i < anchors.length; i++) {
+            anchors[i].style.paddingTop = "57px";
+            anchors[i].style.marginTop = "-57px";
+        }
+    };
+};
+
+// Invoke function to add padding/margins to anchors.
+anchorPadding();
+
+// Catch resize event
+window.addEventListener("resize", anchorPadding);
+
+// Close navbar menu after clicking link
+$('.navbar-collapse a').click(function(){
+    $(".navbar-collapse").collapse('hide');
+});
