7d401e5172b8ebe8f80460fe73fe3c2a8f962d11
Stephane Dorotich <stephanedorotich@gmail.com>
2021-08-18 16:11:39 -0600

    Fixed mistake in parallax background logic

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Position of FAQ section (breakpoint for switching backgrounds)
-var faqPos;
+var faqPos = document.getElementsByName('faq')[0].offsetTop;
 
 // Catch scroll event
 // Swap parallax image backgrounds at FAQ section breakpoint.
@@ -7,17 +7,14 @@ $( window ).scroll(function() {
     // get scroll position
     var scrollPos = $(this).scrollTop();
 
-    // get position of FAQ section
-    if (window.innerWidth < 768) faqPos += 158;
-
     // Check scroll position against FAQ section breakpoint
     if (scrollPos < faqPos) {
         // User hasn't scrolled past FAQ Section. Background 1 visible
-        if ($("bg1").css('visibility') == 'visible') return;
+        if ($("#bg1").css('visibility') == 'visible') return;
         else $("#bg1" ).css( 'visibility', 'visible');
     } else {
         // User has scrolled past FAQ Section. Background 2 visible
-        if ($("bg1").css('visibility') == 'hidden') return;
+        if ($("#bg1").css('visibility') == 'hidden') return;
         else $("#bg1" ).css('visibility', 'hidden');
     }
   });
@@ -26,10 +23,9 @@ $( window ).scroll(function() {
 //  Adjusts padding/margins on anchors to accommodate navbar and navbar toggle
 //  Ensures that links scroll to top of anchored sections
 function anchorPadding() {
-    faqPos = document.getElementsByName('faq')[0].offsetTop;
     var anchors = document.getElementsByClassName("anchor");
     if (window.innerWidth < 768) {
-        faqPos += 158;
+        faqPos = document.getElementsByName('faq')[0].offsetTop + 158;
         for (var i = 0; i < anchors.length; i++) {
             anchors[i].style.paddingTop = "215px";
             anchors[i].style.marginTop = "-215px";
