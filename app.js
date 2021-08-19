478b1df9ab098701e5f8108e8cc7c6de073fdf79
Stephane Dorotich <stephanedorotich@gmail.com>
2021-08-19 13:13:29 -0600

    Replaced janky JS scrolling fix with mobile anchors and mobile only menu. Added nav buttons to homepage. Added favicon.

--- app.js
+++ app.js
@@ -1,45 +1,15 @@
-// Position of FAQ section (breakpoint for switching backgrounds)
-var faqPos = document.getElementsByName('faq')[0].offsetTop;
-
 // Catch scroll event
 // Swap parallax image backgrounds at FAQ section breakpoint.
 $( window ).scroll(function() {
-    // get scroll position
-    var scrollPos = $(this).scrollTop();
-
-    // Check scroll position against FAQ section breakpoint
-    if (scrollPos < faqPos) {
+    // Check scroll position against FAQ section position
+    if ($(this).scrollTop() <= document.getElementsByName('faq')[0].offsetTop) {
         // User hasn't scrolled past FAQ Section. Background 1 visible
-        if ($("#bg1").css('visibility') == 'visible') return;
-        else $("#bg1" ).css( 'visibility', 'visible');
+        $("#bg1" ).css( 'visibility', 'visible');
     } else {
         // User has scrolled past FAQ Section. Background 2 visible
-        if ($("#bg1").css('visibility') == 'hidden') return;
-        else $("#bg1" ).css('visibility', 'hidden');
-    }
-  });
-
-// Anchor padding
-//  Adjusts padding/margins on anchors to accommodate navbar and navbar toggle
-//  Ensures that links scroll to top of anchored sections
-function anchorPadding() {
-    var anchors = document.getElementsByClassName("anchor");
-    if (window.innerWidth < 768) {
-        faqPos = document.getElementsByName('faq')[0].offsetTop + 158;
-        for (var i = 0; i < anchors.length; i++) {
-            anchors[i].style.paddingTop = "215px";
-            anchors[i].style.marginTop = "-215px";
-        }
-    } else {
-        for (var i = 0; i < anchors.length; i++) {
-            anchors[i].style.paddingTop = "57px";
-            anchors[i].style.marginTop = "-57px";
-        }
+        $("#bg1" ).css('visibility', 'hidden');
     };
-}; anchorPadding(); // Invoke to add margins and padding to anchors.
-
-// Catch resize event
-window.addEventListener("resize", anchorPadding);
+});
 
 // Close navbar menu after clicking link
 $('.navbar-collapse a').click(function(){
